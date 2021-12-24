7cf7dbb8279f2cc7295cc5b816225f8009256a9d
Jeremy D. Pretty <70484698+jpretty01@users.noreply.github.com>
2021-12-24 14:37:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#Imports
+from replit import clear
+from art import logo
+print(logo)
+
+#Creating the bids dictionary
+bids = {}
+bidding_finished = False
+
+#This is used to find the highest bidder
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  # going through each bidder
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+#The loop that keeps taking in data until it's over
+while not bidding_finished:
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    clear()
+  
